dd267a35e186552a9123fa3fc76e209e30e95389
Nishada <nishada46@gmail.com>
2022-09-03 16:35:17 +0530

    this is my netflix

---
+++ src/Constants/Constants.js
@@ -0,0 +1,5 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY="03aae4268f6850e5e49a6bbeeec0e55a";
+export const imageUrl = 'https://image.tmdb.org/t/p/original' ; 
+
+
